f6f277c04d824488c98f8ff8b0461da7c44e2a99
MaheshDataWizard <maheshkhatikcareers@gamil.com>
2024-08-09 04:07:59 +0530

    Final Changes

--- script.js
+++ script.js
@@ -10,6 +10,51 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+
+// --------------------------------------Swiper-----------------------------------------
+const swiper = new Swiper('.slider-wrapper', {
+
+    loop: true,
+    autoplay: {
+        delay: 2500,         
+        disableOnInteraction: false,
+      },
+      speed: 600, 
+    grabCursor: true,
+    spaceBetween: 30,
+
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0:{
+            slidesPerView: 1
+        },
+        620:{
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+  
+  });
+
+
+//   -------------------------------Swiper End--------------------------
+
+
 var tablinks=document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
